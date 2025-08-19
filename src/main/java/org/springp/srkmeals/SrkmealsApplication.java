f578f2cf285a371885390056862d713efd2ceec9
Jagdish <Jagadeesh.chaliki@gmail.com>
2025-08-19 13:15:48 +0530

    first commit

---
+++ src/main/java/org/springp/srkmeals/SrkmealsApplication.java
@@ -0,0 +1,14 @@
+package org.springp.srkmeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SrkmealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrkmealsApplication.class, args);
+	}
+
+}
